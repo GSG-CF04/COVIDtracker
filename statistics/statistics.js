ed86a657702abb29002a7cf04611893fa896c8f4
techhana2014 <techhana2014@gmail.com>
2021-12-03 19:18:22 +0200

    Add javascript code for building data list to select countries in statistics page relates#42

--- statistics/statistics.js
+++ statistics/statistics.js
@@ -1,20 +1,25 @@
 /* Definition of API */
 const api ="https://api.ipgeolocation.io/ipgeo?apiKey=cf2be30a619a447897245a7857d48018"; // API location
+const api2 ="https://api.covid19api.com/countries"; // API countries
 /* Definition of DOM TREE */ 
 const countries= document.querySelector("#countries"); //The input of data list
 /* Get data from API */
 async function getData(){
-var data;
+var data, data2;
     try{ const response = await fetch(api);// Get data from API location
+        const response2 = await fetch(api2); // Get data from API country   
         data = await response.json();
-        printData(data);  // Call printData function
+        data2 = await response2.json(); 
+        printData(data,data2);  // Call printData function
     }catch (error) {
         alert("ERROR: NO DATA")
         console.warn("Stupid network Error");
     }
 }
 /* print data from API to input field and list */
-function printData(data){
+function printData(data,data2){
 countries.value = data.country_name; //Add current user country in input field relates to IP location
+/* PRINT DATALIST*/
+countries.innerHTML =`<datalist  id="country"><option value="${data.country_name}">${data.country_name}</option>${data2.map( countriess => `<option vlaue="${countriess.ISO2}">${countriess.Country}</option>`)}</datalist>`
 }
 getData();// Call getData function
