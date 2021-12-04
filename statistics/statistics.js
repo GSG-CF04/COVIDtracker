7cb1fd2955922646da35138118484c4d308d6ee3
techhana2014 <techhana2014@gmail.com>
2021-12-04 23:46:39 +0200

    create dark mood for statistics page relates#32

--- statistics/statistics.js
+++ statistics/statistics.js
@@ -27,3 +27,35 @@ countries.value = data.country_name; //Add current user country in input field r
 countries.innerHTML =`<datalist  id="country"><option value="${data.country_name}">${data.country_name}</option>${data2.map( countriess => `<option vlaue="${countriess.ISO2}">${countriess.Country}</option>`)}</datalist>`
 }
 getData();// Call getData function
+//! DARK AND LIGHT THEME TOGGLE
+
+//? calling and accessing the elements
+let toggleBtn = document.querySelector('.toggle-btn')
+let bodyElement = document.querySelector('body')
+
+// ? toggle the class (set and remove the class dark in every click)
+function setDarkTheme(){
+bodyElement.classList.toggle('dark')
+}
+// ? add event listener to the btn
+toggleBtn.addEventListener('click', switchTheme)
+
+function switchTheme() {
+// Get the value of the "dark" item from the local storage on every click
+let darkMode = localStorage.getItem('dark')
+if (darkMode !== 'on') {
+//   Set the value of the item to "on" when dark mode is on
+    setDarkTheme()
+darkMode = localStorage.setItem('dark', 'on')
+} else {
+//   Set the value of the item to  "null" when dark mode if off
+    setDarkTheme()
+darkMode = localStorage.setItem('dark', 'off')
+}
+}
+// Get the value of the "dark" item from the local storage
+let darkMode = localStorage.getItem('dark')
+// check dark mode is on or off on page reload
+if(darkMode === 'on'){
+    setDarkTheme()
+}
