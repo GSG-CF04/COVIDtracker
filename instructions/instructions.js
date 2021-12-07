86beb8c67df0946e304dede89a9aada16a864104
yazan05 <yazanshubair05@gmail.com>
2021-12-07 02:06:11 +0200

    fix dark mode , add responsive desgin

--- instructions/instructions.js
+++ instructions/instructions.js
@@ -1,14 +1,32 @@
-//! DARK AND LIGHT THEME TOGGLE
+// DARK AND LIGHT THEME TOGGLE
+let icon = document.getElementById("icon");
+let icon1 = document.getElementById("icon1");
+let icon2 = document.getElementById("icon2");
+let icon3 = document.getElementById("icon3");
 
-let icon = document.getElementById("icon")
-icon.onclick = function(){
-        document.body.classList.toggle("dark-theme")
-        if(document.body.classList.contains("dark-theme")){
-                icon.src="../images/moon.png";}
+icon.onclick = function() {
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+        icon.src = "../images/moon.png";
+    }
+};
+icon1.onclick = function() {
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+        icon1.src = "../images/moon.png";
+    }
+};
 
-                else{
-                        icon.src="../images/moon.png";
-                } 
-        }
+icon2.onclick = function() {
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+        icon2.src = "../images/moon.png";
+    }
+};
 
-
+icon3.onclick = function() {
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+        icon3.src = "../images/moon.png";
+    }
+};
